7b97cc925606c9bca4b507a4c111d68e3db67e8c
SebastianArango1222 <xebaxlolgamer@gmail.com>
2020-08-06 13:07:50 -0500

    Estoy subiendo la tareas de la guía número 10

---
+++ build/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <table border="5"> 
+      <tr>
+        <td>Nombre</td>
+        <td>Apellido</td>
+        <td>Correo</td>
+        <td>Celular</td>
+      </tr>
+      <tr>
+        <td>Sebastian</td>
+        <td>Arango Diaz</td>
+        <td>sebastianarango1222@gmail.com</td>
+        <td>3002581161</td>
+      </tr>
+      <tr>
+        <td>Miguel Angel</td>
+        <td>Pérez Rojas</td>
+        <td>micperezrojas@gmail.com</td>
+        <td>3013378355</td>
+      </tr>
+      <tr>
+        <td>Juan Sebastian</td>
+        <td>Suarez Lemus</td>
+        <td>omgdelsur@gmail.com</td>
+        <td>3104003423</td>
+      </tr>
+      <tr>
+        <td>Luisa Fernanda</td>
+        <td>Alvarez Diaz</td>
+        <td>luisa2004al@gmail.com</td>
+        <td>Indefinido</td>
+      </tr>
+  </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
